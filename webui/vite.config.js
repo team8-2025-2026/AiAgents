34063ff48ec0f8b678659a1e2308f5078bcbe4a1
Alebik0 <belash.aleksandr777@gmail.com>
2025-12-07 21:16:44 +0300

    Base docker compose implementation

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -5,14 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
 })
 
 
